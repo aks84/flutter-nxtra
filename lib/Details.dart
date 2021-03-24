9d7c778a738f773dabdc949784db74b512bd6715
Amulya Kumar Shahi <amulyaa_24@yahoo.in>
2021-03-24 22:47:00 +0530

    released 4th

--- lib/Details.dart
+++ lib/Details.dart
@@ -30,16 +30,17 @@ class _DetailsState extends State<Details>{
 
           Container(
             padding: EdgeInsets.all(5.0),
+            color: Colors.brown[100],
             child: Text("Nakshatra : "+widget.jsonDetails['nakshatra'], style: const TextStyle(
               fontWeight: FontWeight.w500,
-              fontSize: 15.0,
+              fontSize: 25.0,
             ),
             ),
           ),
 
 
           Container(
-            padding: EdgeInsets.all(5),
+            padding: EdgeInsets.all(6),
             child:
             Text("Deity : "+widget.jsonDetails['deity'], style: const TextStyle(
               fontWeight: FontWeight.w500,
@@ -49,7 +50,7 @@ class _DetailsState extends State<Details>{
           ),
 
           Container(
-            padding: EdgeInsets.all(5),
+            padding: EdgeInsets.all(6),
             child: Text("Planet Lord : "+widget.jsonDetails['lord'], style: const TextStyle(
               fontWeight: FontWeight.w500,
               fontSize: 15.0,
@@ -58,7 +59,7 @@ class _DetailsState extends State<Details>{
           ),
 
           Container(
-            padding: EdgeInsets.all(5),
+            padding: EdgeInsets.all(6),
             child:
             Text("Gender : "+widget.jsonDetails['Gender'], style: const TextStyle(
               fontWeight: FontWeight.w500,
@@ -66,8 +67,19 @@ class _DetailsState extends State<Details>{
             ),
             ),
           ),
+
           Container(
-            padding: EdgeInsets.all(5),
+            padding: EdgeInsets.all(6),
+            child:
+            Text("Caste : "+widget.jsonDetails['caste'], style: const TextStyle(
+              fontWeight: FontWeight.w500,
+              fontSize: 15.0,
+            ),
+            ),
+          ),
+
+          Container(
+            padding: EdgeInsets.all(6),
             child:
             Text("Element : "+widget.jsonDetails['element'], style: const TextStyle(
               fontWeight: FontWeight.w500,
@@ -77,7 +89,7 @@ class _DetailsState extends State<Details>{
           ),
 
           Container(
-            padding: EdgeInsets.all(5),
+            padding: EdgeInsets.all(6),
             child: Text("Aim : "+widget.jsonDetails['aim'], style: const TextStyle(
               fontWeight: FontWeight.w500,
               fontSize: 15.0,
@@ -85,17 +97,20 @@ class _DetailsState extends State<Details>{
             ),
           ),
 
+          Divider(),
+
           Container(
-            padding: EdgeInsets.all(5),
+            padding: EdgeInsets.all(6),
+            color: Colors.brown[100],
             child: Text("Falls in : "+widget.jsonDetails['falls'], style: const TextStyle(
               fontWeight: FontWeight.w500,
-              fontSize: 15.0,
+              fontSize: 20.0,
             ),
             ),
           ),
 
           Container(
-            padding: EdgeInsets.all(5),
+            padding: EdgeInsets.all(6),
             child: Text("Pada 1 : "+widget.jsonDetails['pada1'], style: const TextStyle(
               fontWeight: FontWeight.w500,
               fontSize: 15.0,
@@ -103,7 +118,7 @@ class _DetailsState extends State<Details>{
             ),
           ),
           Container(
-            padding: EdgeInsets.all(5),
+            padding: EdgeInsets.all(6),
             child: Text("Pada 2 : "+widget.jsonDetails['pada2'], style: const TextStyle(
               fontWeight: FontWeight.w500,
               fontSize: 15.0,
@@ -111,7 +126,7 @@ class _DetailsState extends State<Details>{
             ),
           ),
           Container(
-            padding: EdgeInsets.all(5),
+            padding: EdgeInsets.all(6),
             child: Text("Pada 3 : "+widget.jsonDetails['pada3'], style: const TextStyle(
               fontWeight: FontWeight.w500,
               fontSize: 15.0,
@@ -119,7 +134,7 @@ class _DetailsState extends State<Details>{
             ),
           ),
           Container(
-            padding: EdgeInsets.all(5),
+            padding: EdgeInsets.all(6),
             child: Text("Pada 4 : "+widget.jsonDetails['pada4'], style: const TextStyle(
               fontWeight: FontWeight.w500,
               fontSize: 15.0,
@@ -127,31 +142,21 @@ class _DetailsState extends State<Details>{
             ),
           ),
 
-
-
-          Container(
-            padding: EdgeInsets.all(5),
-            child:
-            Text("Caste : "+widget.jsonDetails['caste'], style: const TextStyle(
-              fontWeight: FontWeight.w500,
-              fontSize: 15.0,
-            ),
-            ),
-          ),
-
+        Divider(),
 
         Container(
-          padding: EdgeInsets.all(5),
+          padding: EdgeInsets.all(6),
+          color: Colors.brown[100],
           child:
           Text("Symbol : "+widget.jsonDetails['symbol'], style: const TextStyle(
             fontWeight: FontWeight.w500,
-            fontSize: 15.0,
+            fontSize: 20.0,
           ),
           ),
         ),
 
           Container(
-            padding: EdgeInsets.all(5),
+            padding: EdgeInsets.all(6),
             child: Text("Keywords : "+widget.jsonDetails['keywords'], style: const TextStyle(
               fontWeight: FontWeight.w500,
               fontSize: 15.0,
@@ -162,7 +167,7 @@ class _DetailsState extends State<Details>{
 
 
         Container(
-          padding: EdgeInsets.all(5),
+          padding: EdgeInsets.all(6),
           child:
           Text("Direction : "+widget.jsonDetails['direction'], style: const TextStyle(
             fontWeight: FontWeight.w500,
@@ -172,7 +177,7 @@ class _DetailsState extends State<Details>{
         ),
 
         Container(
-          padding: EdgeInsets.all(5),
+          padding: EdgeInsets.all(6),
           child:
           Text("Power : "+widget.jsonDetails['power'], style: const TextStyle(
             fontWeight: FontWeight.w500,
@@ -182,7 +187,7 @@ class _DetailsState extends State<Details>{
         ),
 
           Container(
-            padding: EdgeInsets.all(5),
+            padding: EdgeInsets.all(6),
             child:
             Text("Sounds : "+widget.jsonDetails['sounds'], style: const TextStyle(
               fontWeight: FontWeight.w500,
@@ -190,6 +195,119 @@ class _DetailsState extends State<Details>{
             ),
             ),
           ),
+
+          Divider(),
+          Container(
+            padding: EdgeInsets.all(10),
+            color: Colors.brown[100],
+            child:
+            Text("If a planet falls in "+widget.jsonDetails['nakshatra']+" you should expect the followings from the native.", style: const TextStyle(
+              fontWeight: FontWeight.w500,
+              fontSize: 18.0,
+            ),
+            ),
+          ),
+          Divider(),
+
+          Container(
+            padding: EdgeInsets.all(6),
+            child:
+            Text("Ascendant : "+widget.jsonDetails['ascendant'], style: const TextStyle(
+              fontWeight: FontWeight.w500,
+              fontSize: 15.0,
+            ),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(6),
+            child:
+            Text("Sun : "+widget.jsonDetails['sun'], style: const TextStyle(
+              fontWeight: FontWeight.w500,
+              fontSize: 15.0,
+            ),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(6),
+            child:
+            Text("Moon : "+widget.jsonDetails['moon'], style: const TextStyle(
+              fontWeight: FontWeight.w500,
+              fontSize: 15.0,
+            ),
+            ),
+          ),
+
+          Container(
+            padding: EdgeInsets.all(6),
+            child:
+            Text("Mercury : "+widget.jsonDetails['mercury'], style: const TextStyle(
+              fontWeight: FontWeight.w500,
+              fontSize: 15.0,
+            ),
+            ),
+          ),
+
+          Container(
+            padding: EdgeInsets.all(6),
+            child:
+            Text("Venus : "+widget.jsonDetails['venus'], style: const TextStyle(
+              fontWeight: FontWeight.w500,
+              fontSize: 15.0,
+            ),
+            ),
+          ),
+
+          Container(
+            padding: EdgeInsets.all(6),
+            child:
+            Text("Mars : "+widget.jsonDetails['mars'], style: const TextStyle(
+              fontWeight: FontWeight.w500,
+              fontSize: 15.0,
+            ),
+            ),
+          ),
+
+          Container(
+            padding: EdgeInsets.all(6),
+            child:
+            Text("Saturn : "+widget.jsonDetails['saturn'], style: const TextStyle(
+              fontWeight: FontWeight.w500,
+              fontSize: 15.0,
+            ),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(6),
+            child:
+            Text("Jupiter : "+widget.jsonDetails['jupiter'], style: const TextStyle(
+              fontWeight: FontWeight.w500,
+              fontSize: 15.0,
+            ),
+            ),
+          ),
+
+          Container(
+            padding: EdgeInsets.all(6),
+            child:
+            Text("Rahu : "+widget.jsonDetails['rahu'], style: const TextStyle(
+              fontWeight: FontWeight.w500,
+              fontSize: 15.0,
+            ),
+            ),
+          ),
+
+          Container(
+            padding: EdgeInsets.all(6),
+            child:
+            Text("Ketu : "+widget.jsonDetails['ketu'], style: const TextStyle(
+              fontWeight: FontWeight.w500,
+              fontSize: 15.0,
+            ),
+            ),
+          ),
+
+
+
         ],
 
       )
--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:nakshtras/Homepage.dart';
 void main()=> runApp(new MaterialApp(
   theme: new ThemeData(
-    primarySwatch: Colors.teal,
+    primarySwatch: Colors.brown,
   ),
   debugShowCheckedModeBanner: false,
   home: new HomePage(),
